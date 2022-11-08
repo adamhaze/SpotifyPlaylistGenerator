23a35bc905e6678df14c02405fb5eb3a8aaff04e
Evan Easton <denvereastons3@gmail.com>
2022-11-08 16:02:59 -0700

    test for axios requests

--- src/main/java/spg/mongodb/UserController.java
+++ src/main/java/spg/mongodb/UserController.java
@@ -2,6 +2,7 @@ package spg.mongodb;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,10 +12,13 @@ public class UserController {
     @Autowired
     private UserRepository repository;
 
-    @GetMapping("/users")
-    public void saveUserToDB() {
-        // repository.deleteAll();
+    @CrossOrigin(origins = "http://localhost:3000")
+    @GetMapping(path="/users",  produces = "text/plain;charset=UTF-8")
+    public String saveUserToDB() {
+        repository.deleteAll();
         repository.save(new User("Adam", "adamhaze", "mypassword", "adamhayes1515@gmail.com"));
+
+        return "this is a test";
     }
 
 }
---
+++ ui/src/RouteController.js
@@ -0,0 +1,18 @@
+const axios = require('axios').default;
+
+export function createUser(obj){
+
+    axios.get('http://localhost:8080/users')
+        .then(function (response) {
+            // handle success
+            console.log(response);
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        })
+        .then(function () {
+            // always executed
+        });
+
+}
