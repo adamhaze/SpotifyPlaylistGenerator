b6e065ade7f0b76f82ee49fc66f0c91db6c3d70e
Evan Easton <denvereastons3@gmail.com>
2022-11-16 12:21:19 -0700

    updated create account

--- src/main/java/spg/mongodb/UserController.java
+++ src/main/java/spg/mongodb/UserController.java
@@ -1,6 +1,8 @@
 package spg.mongodb;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -13,10 +15,15 @@ public class UserController {
 
     @CrossOrigin(origins = "http://localhost:3000")
     @PostMapping(path="/users")
-    public User saveUserToDB(@Validated @RequestBody User user) {
+    public ResponseEntity<String> saveUserToDB(@Validated @RequestBody User user) {
         System.out.println(user);
-        repository.deleteAll();
-        return repository.save(user);
+        //repository.deleteAll();
+        if(repository.findFirstByEmail(user.email) != null) {
+            return new ResponseEntity<>("User already exists in database", HttpStatus.FORBIDDEN);
+        }else{
+            repository.save(user);
+            return new ResponseEntity<>("Success", HttpStatus.CREATED);
+        }
     }
 
 }
