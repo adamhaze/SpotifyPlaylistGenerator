0908594b25505b26f63be416ec687c89789e456a
Evan Easton <denvereastons3@gmail.com>
2022-11-16 12:41:10 -0700

    RouteController changes

--- ui/src/RouteController.js
+++ ui/src/RouteController.js
@@ -1,30 +1,40 @@
 const axios = require('axios').default;
 
-export function createUser(obj){
+export async function createUser(obj){
 
     console.log(obj)
-
-    axios.post('http://localhost:8080/users', obj)
-        .then(function (response) {
-            // handle success
-            console.log(response);
-        })
-        .catch(function (error) {
-            // handle error
-            console.log(error);
-        })
-        .then(function () {
-            // always executed
-        });
-
+    //
+    // axios.post('http://localhost:8080/users', obj)
+    //     .then(function (response) {
+    //         // handle success
+    //         console.log(response);
+    //     })
+    //     .catch(function (error) {
+    //         // handle error
+    //         console.log(error);
+    //     })
+    //     .then(function () {
+    //         // always executed
+    //     });
+    try{
+        const response = await axios.post('http://localhost:8080/users', obj);
+        console.log(response);
+    }catch (error){
+        console.log(error);
+    }
+    return "";
 }
 
 
 export async function validateLogin(obj) {
-    console.log("in validateLogin...")
-    const response = axios.post('http://localhost:8080/login', {
-        'username': obj.username,
-        'password': obj.password
-    })
-    return (await response).data;
+    try {
+        console.log("in validateLogin...")
+        const response = await axios.post('http://localhost:8080/login', {
+            'username': obj.username,
+            'password': obj.password
+        })
+        return response.data;
+    }catch(error){
+        return error.data;
+    }
 }
